6f7e7c0c6aeca8017a069ab54b992a70538d26cd
Felipe Fronchetti <fronchettl@vcu.edu>
2022-01-20 15:53:39 -0500

    First version of canvas containing all the necessary information.
    The canvas containing data from the eye tracking system is finally ready for testing. This is still a draft though.

--- Assets/Scripts/GetEyeTrackingValues.cs
+++ Assets/Scripts/GetEyeTrackingValues.cs
@@ -7,284 +7,92 @@ using UnityEngine.XR;
 using UnityEngine.UI;
 using Varjo.XR;
 
-public enum GazeDataSource
-{
-    InputSubsystem,
-    GazeAPI
-}
-
 public class GetEyeTrackingValues : MonoBehaviour
 {
-    [Header("XR Camera")]
-    public Camera XRCamera;
-
-    [Header("Gaze Point Indicator")]
-    public GameObject gazeTarget;
-
-    [Header("Visualization Transforms")]
-    public Transform fixationPointTransform;
-    public Transform leftEyeTransform;
-    public Transform rightEyeTransform;
-
-    [Header("Data Source")]
-    public GazeDataSource gazeDataSource = GazeDataSource.InputSubsystem;
-
+    // Settings
     [Header("Calibration Settings")]
-    public VarjoEyeTracking.GazeCalibrationMode gazeCalibrationMode = VarjoEyeTracking.GazeCalibrationMode.Fast;
+    public VarjoEyeTracking.GazeCalibrationMode calibrationMode = VarjoEyeTracking.GazeCalibrationMode.Fast;
     public KeyCode calibrationRequestKey = KeyCode.Space;
 
-    [Header("Output Filter Settings")]
-    public VarjoEyeTracking.GazeOutputFilterType gazeOutputFilterType = VarjoEyeTracking.GazeOutputFilterType.Standard;
-    public KeyCode setOutputFilterTypeKey = KeyCode.RightShift;
+    [Header("XR camera")]
+    public Camera XRCamera;
 
-    [Header("Target Visibility (Toggle)")]
-    public KeyCode toggleGazeTarget = KeyCode.Return;
-
-    [Header("Fixation Point Indicator (Toggle)")]
-    public bool showFixationPoint = true;
-
-    [Header("Gaze Ray (Radius)")]
-    public float gazeRadius = 0.01f;
-
-    [Header("Gaze Point (Distance)")]
-    public float floatingGazeTargetDistance = 5f;
-
-    [Header("Gaze Target Towards Viewer (Offset)")]
-    public float targetOffset = 0.2f;
-
-    [Header("Hit Force At Looking Point")]
-    public float hitForce = 5f;
-
-    [Header("Debug Keys")]
-    public KeyCode checkGazeAllowed = KeyCode.PageUp;
-    public KeyCode checkGazeCalibrated = KeyCode.PageDown;
-
-    [Header("Text Components (Canvas)")]
+    // Text Components
+    [Header("Display (Text Components)")]
+    // Display
+    public Text displayPosition;
+    public Text displayRotation;
+    [Header("Gaze (Text Components)")]
+    // Gaze
     public Text gazeStatus;
-    public Text eyesStatus;
-    public Text leftEyePositionComponent;
-    public Text rightEyePositionComponent;
+    public Text gazePosition;
+    public Text gazeForward;
+    [Header("Left Eye (Text Components)")]
+    // Left Eye
+    public Text leftEyeStatus;
+    public Text leftEyePosition;
+    public Text leftEyeForward;
+    public Text leftEyePupilSize;
+    [Header("Right Eye (Text Components)")]
+    // Right Eye
+    public Text rightEyeStatus;
+    public Text rightEyePosition;
+    public Text rightEyeForward;
+    public Text rightEyePupilSize;
+    [Header("Focus (Text Components)")]
+    // Focus
+    public Text focusDistance;
+    public Text focusStability;
+    [Header("Capture Tiem (Text Component)")]
+    // Focus
+    public Text captureTime;
 
-    private List<InputDevice> devices = new List<InputDevice>();
 
-    private InputDevice device;
-    private Eyes eyes;
-
-    private VarjoEyeTracking.GazeData gazeData;
-    private List<VarjoEyeTracking.GazeData> dataSinceLastUpdate;
-
-    private Vector3 leftEyePosition;
-    private Vector3 rightEyePosition;
-
-    private Quaternion leftEyeRotation;
-    private Quaternion rightEyeRotation;
-
-    private Vector3 fixationPoint;
-    private Vector3 direction;
-    private Vector3 rayOrigin;
-
-    private RaycastHit hit;
-
-    private float distance;
-
-    void GetDevice()
-    {
-        InputDevices.GetDevicesAtXRNode(XRNode.CenterEye, devices);
-        device = devices.FirstOrDefault();
-    }
-
-    void OnEnable()
-    {
-        if (!device.isValid)
-        {
-            GetDevice();
-        }
-    }
+    private VarjoEyeTracking.GazeData latestGazeCapture;
 
     private void Start()
     {
-        //Hiding the gazetarget if gaze is not available or if the gaze calibration is not done
-        if (VarjoEyeTracking.IsGazeAllowed() && VarjoEyeTracking.IsGazeCalibrated())
-        {
-            gazeTarget.SetActive(true);
-        }
-        else
-        {
-            gazeTarget.SetActive(false);
-        }
-
-        if (showFixationPoint)
-        {
-            fixationPointTransform.gameObject.SetActive(true);
-        }
-        else
-        {
-            fixationPointTransform.gameObject.SetActive(false);
-        }
+        // Request calibration every time the program starts
+        VarjoEyeTracking.RequestGazeCalibration(calibrationMode);
     }
 
     void Update()
     {
-        // Request gaze calibration
+        // If calibration key is pressed, a calibration request is sent to headset
         if (Input.GetKeyDown(calibrationRequestKey))
         {
-            VarjoEyeTracking.RequestGazeCalibration(gazeCalibrationMode);
+            VarjoEyeTracking.RequestGazeCalibration(calibrationMode);
         }
 
-        // Set output filter type
-        if (Input.GetKeyDown(setOutputFilterTypeKey))
-        {
-            VarjoEyeTracking.SetGazeOutputFilterType(gazeOutputFilterType);
-            Debug.Log("Gaze output filter type is now: " + VarjoEyeTracking.GetGazeOutputFilterType());
-        }
-
-        // Check if gaze is allowed
-        if (Input.GetKeyDown(checkGazeAllowed))
-        {
-            Debug.Log("Gaze allowed: " + VarjoEyeTracking.IsGazeAllowed());
-        }
-
-        // Check if gaze is calibrated
-        if (Input.GetKeyDown(checkGazeCalibrated))
-        {
-            Debug.Log("Gaze calibrated: " + VarjoEyeTracking.IsGazeCalibrated());
-        }
-
-        // Toggle gaze target visibility
-        if (Input.GetKeyDown(toggleGazeTarget))
-        {
-            gazeTarget.GetComponentInChildren<MeshRenderer>().enabled = !gazeTarget.GetComponentInChildren<MeshRenderer>().enabled;
-        }
-
-        // Get gaze data if gaze is allowed and calibrated
-        if (VarjoEyeTracking.IsGazeAllowed() && VarjoEyeTracking.IsGazeCalibrated())
-        {
-            //Get device if not valid
-            if (!device.isValid)
-            {
-                GetDevice();
-            }
-
-            // Show gaze target
-            gazeTarget.SetActive(true);
-
-            if (gazeDataSource == GazeDataSource.InputSubsystem)
-            {
-                // Get data for eye positions, rotations and the fixation point
-                if (device.TryGetFeatureValue(CommonUsages.eyesData, out eyes))
-                {
-                    if (eyes.TryGetLeftEyePosition(out leftEyePosition))
-                    {
-                        leftEyeTransform.localPosition = leftEyePosition;
-                    }
-
-                    if (eyes.TryGetLeftEyeRotation(out leftEyeRotation))
-                    {
-                        leftEyeTransform.localRotation = leftEyeRotation;
-                    }
-
-                    if (eyes.TryGetRightEyePosition(out rightEyePosition))
-                    {
-                        rightEyeTransform.localPosition = rightEyePosition;
-                    }
-
-                    if (eyes.TryGetRightEyeRotation(out rightEyeRotation))
-                    {
-                        rightEyeTransform.localRotation = rightEyeRotation;
-                    }
-
-                    if (eyes.TryGetFixationPoint(out fixationPoint))
-                    {
-                        fixationPointTransform.localPosition = fixationPoint;
-                    }
-                }
-
-                // Set raycast origin point to VR camera position
-                rayOrigin = XRCamera.transform.position;
-
-                // Direction from VR camera towards fixation point
-                direction = (fixationPointTransform.position - XRCamera.transform.position).normalized;
-
-            }
-            else
-            {
-                gazeData = VarjoEyeTracking.GetGaze();
-
-                if (gazeData.status != VarjoEyeTracking.GazeStatus.Invalid)
-                {
-                    // GazeRay vectors are relative to the HMD pose so they need to be transformed to world space
-                    if (gazeData.leftStatus != VarjoEyeTracking.GazeEyeStatus.Invalid)
-                    {
-                        leftEyeTransform.position = XRCamera.transform.TransformPoint(gazeData.left.origin);
-                        leftEyeTransform.rotation = Quaternion.LookRotation(XRCamera.transform.TransformDirection(gazeData.left.forward));
-                    }
-
-                    if (gazeData.rightStatus != VarjoEyeTracking.GazeEyeStatus.Invalid)
-                    {
-                        rightEyeTransform.position = XRCamera.transform.TransformPoint(gazeData.right.origin);
-                        rightEyeTransform.rotation = Quaternion.LookRotation(XRCamera.transform.TransformDirection(gazeData.right.forward));
-                    }
-
-                    // Set gaze origin as raycast origin
-                    rayOrigin = XRCamera.transform.TransformPoint(gazeData.gaze.origin);
-
-                    // Set gaze direction as raycast direction
-                    direction = XRCamera.transform.TransformDirection(gazeData.gaze.forward);
-
-                    // Fixation point can be calculated using ray origin, direction and focus distance
-                    fixationPointTransform.position = rayOrigin + direction * gazeData.focusDistance;
-                }
-            }
-        }
-
-        // Raycast to world from VR Camera position towards fixation point
-        if (Physics.SphereCast(rayOrigin, gazeRadius, direction, out hit))
-        {
-            // Put target on gaze raycast position with offset towards user
-            gazeTarget.transform.position = hit.point - direction * targetOffset;
-
-            // Make gaze target point towards user
-            gazeTarget.transform.LookAt(rayOrigin, Vector3.up);
-
-            // Scale gazetarget with distance so it apperas to be always same size
-            distance = hit.distance;
-            gazeTarget.transform.localScale = Vector3.one * distance;
-
-            // Alternative way to check if you hit object with tag
-            if (hit.transform.CompareTag("FreeRotating"))
-            {
-                AddForceAtHitPosition();
-            }
-        }
-        else
-        {
-            // If gaze ray didn't hit anything, the gaze target is shown at fixed distance
-            gazeTarget.transform.position = rayOrigin + direction * floatingGazeTargetDistance;
-            gazeTarget.transform.LookAt(rayOrigin, Vector3.up);
-            gazeTarget.transform.localScale = Vector3.one * floatingGazeTargetDistance;
-        }
-
-        int dataCount = VarjoEyeTracking.GetGazeList(out dataSinceLastUpdate);
-
-        foreach (var data in dataSinceLastUpdate)
-        {
-            updateCanvas(data);
-        }
+        // Get data from the latest frame captured by the eye
+        // tracking system
+        latestGazeCapture = VarjoEyeTracking.GetGaze();
+        updateCanvas(latestGazeCapture);
     }
 
-    void updateCanvas(VarjoEyeTracking.GazeData data)
+    void updateCanvas(VarjoEyeTracking.GazeData gazeCapture)
     {
-        leftEyePositionComponent.text = "Left Eye Position: " + leftEyePosition + " " + leftEyeRotation;
-        rightEyePositionComponent.text = "Right Eye Position: " + rightEyePosition + " " + rightEyeRotation;
-    }
-
-    void AddForceAtHitPosition()
-    {
-        //Get Rigidbody form hit object and add force on hit position
-        Rigidbody rb = hit.rigidbody;
-        if (rb != null)
-        {
-            rb.AddForceAtPosition(direction * hitForce, hit.point, ForceMode.Force);
-        }
+        /* Display */
+        displayPosition.text = "Position: " + XRCamera.transform.localPosition.ToString("F3");
+        displayRotation.text = "Rotation: " + XRCamera.transform.localRotation.ToString("F3");
+        /* Gaze */
+        gazeStatus.text = "Status: " + gazeCapture.status;
+        gazePosition.text = "Position (Origin): " + gazeCapture.gaze.origin.ToString("F3");
+        gazeForward.text = "Position (Forward): " + gazeCapture.gaze.forward.ToString("F3");
+        /* Left Eye */
+        leftEyeStatus.text = "Status: " + gazeCapture.leftStatus;
+        leftEyePosition.text = "Position (Origin): " + gazeCapture.left.origin.ToString("F3");
+        leftEyeForward.text = "Position (Forward): " + gazeCapture.left.forward.ToString("F3");
+        leftEyePupilSize.text = "Pupil Size: " + gazeCapture.leftPupilSize.ToString();
+        /* Right Eye */
+        rightEyeStatus.text = "Status: " + gazeCapture.rightStatus;
+        rightEyePosition.text = "Position (Origin): " + gazeCapture.right.origin.ToString("F3");
+        rightEyeForward.text = "Position (Forward): " + gazeCapture.right.forward.ToString("F3");
+        rightEyePupilSize.text = "Pupil Size: " + gazeCapture.rightPupilSize.ToString();
+        /* Focus */
+        focusDistance.text = "Distance: " + gazeCapture.focusDistance.ToString();
+        focusStability.text = "Stability: " + gazeCapture.focusStability.ToString();
+        /* Capture Time */
+        captureTime.text = "Capture Time: " + (DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond).ToString();
     }
 }
